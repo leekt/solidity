c09a8c1c734042b37b3d776942383787d64e864a
rodiazet <radek.zagorowicz@gmail.com>
2026-05-07 15:18:54 +0200

    Add memoization to SSAValueTracker::isSSAWithDependencies

--- libyul/optimiser/SSAValueTracker.cpp
+++ libyul/optimiser/SSAValueTracker.cpp
@@ -64,7 +64,12 @@ bool SSAValueTracker::isSSAWithDependencies(Expression const* _expression) const
 	if (_expression == nullptr)
 		return true;
 
-	return std::visit(
+	// Check cache first
+	auto cacheIt = m_isSSACache.find(_expression);
+	if (cacheIt != m_isSSACache.end())
+		return cacheIt->second;
+
+	bool const result = std::visit(
 		util::GenericVisitor{
 			[&](FunctionCall const& _call) {
 				for (auto const& argument: _call.arguments)
@@ -80,6 +85,9 @@ bool SSAValueTracker::isSSAWithDependencies(Expression const* _expression) const
 		},
 		*_expression
 	);
+
+	m_isSSACache[_expression] = result;
+	return result;
 }
 
 std::set<YulName> SSAValueTracker::ssaVariables(Block const& _ast)
